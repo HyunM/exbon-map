df5653266dea78515a685970743950685db31447
HyunM <wisek0226@gmail.com>
2021-08-04 07:11:39 -0700

    apiKey gitIgnore

--- pages/index.js
+++ pages/index.js
@@ -1,24 +1,36 @@
 import GoogleMapReact from "google-map-react";
+import { apiKey } from "../apiKey.js";
 
 export default function Index() {
   const ex1 = {
     center: {
-      lat: 59.95,
-      lng: 30.33,
+      lat: 33.76176289096943,
+      lng: -117.92945707982629,
     },
     zoom: 11,
   };
-
-  const AnyReactComponent = ({ text }) => <div>{text}</div>;
+  const AnyReactComponent = ({ text }) => (
+    <div style={{ color: "red" }}>{text}</div>
+  );
 
   return (
     <div style={{ height: "100vh", width: "100%" }}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: "AIzaSyAHIuYBU49zzZldCGffztJ1lPo4F9whN5I" }}
+        bootstrapURLKeys={{ key: apiKey }}
         defaultCenter={ex1.center}
         defaultZoom={ex1.zoom}
       >
-        <AnyReactComponent lat={59.955413} lng={30.337844} text="My Marker" />
+        <AnyReactComponent
+          lat={33.76176289096943}
+          lng={-117.92945707982629}
+          text="Project1"
+        />
+
+        <AnyReactComponent
+          lat={33.759963306897774}
+          lng={-117.92870727216075}
+          text="Project2"
+        />
       </GoogleMapReact>
     </div>
   );
