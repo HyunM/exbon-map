e683f32db3c1dab533956caabb070c142e9aeb30
HyunM <wisek0226@gmail.com>
2021-08-13 15:23:15 -0700

    api call success

---
+++ pages/api/project-same-address.js
@@ -0,0 +1,39 @@
+const mssql = require("mssql");
+const dbserver = require("../../dbConfig.js");
+
+const projectSameAddress = (req, res) => {
+  const { method, body } = req;
+  return new Promise(resolve => {
+    switch (method) {
+      case "GET":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+
+          const query = `EXEC [Hammer].[dbo].[Map_ProjectSameAddress_Select]`;
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            const response = recordset.recordset;
+            res.status(200).json(response);
+            return resolve();
+          });
+        });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(404).end(`Failed`);
+        resolve();
+    }
+  });
+};
+
+export default projectSameAddress;
