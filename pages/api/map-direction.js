c6dfe70c5c283ea9dbdf73bf7cc8406987538518
HyunM <wisek0226@gmail.com>
2021-10-05 10:54:09 -0700

    api call success

---
+++ pages/api/map-direction.js
@@ -0,0 +1,42 @@
+const mssql = require("mssql");
+const dbserver = require("../../dbConfig.js");
+
+const projectInfo = (req, res) => {
+  const { method, body } = req;
+  return new Promise(resolve => {
+    switch (method) {
+      case "GET":
+        mssql.connect(dbserver.dbConfig, err => {
+          if (err) {
+            console.error(err);
+            return resolve();
+          }
+          const request = new mssql.Request();
+          const selectedDate = req.query.selectedDate;
+          const employeeID = req.query.EmployeeID;
+
+          const query = `EXEC [Hammer].[dbo].[Map_Direction]
+          ${employeeID}, '${selectedDate}'`;
+
+          request.query(query, (err, recordset) => {
+            if (err) {
+              console.error(err);
+              return resolve();
+            }
+            const response = recordset.recordset;
+            res.status(200).json(response);
+            return resolve();
+          });
+        });
+        break;
+
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+        res.status(404).end(`Failed`);
+        resolve();
+    }
+  });
+};
+
+export default projectInfo;
